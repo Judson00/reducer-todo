6267a13aaed87129f41e940a9e633d90e990413f
Robert J. Mitchell <robert.judson@outlook.com>
2020-01-22 01:57:15 -0500

    got it working, now all it needs is styling and testing

--- todo/src/components/App.js
+++ todo/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
-import VisibleTodoList from '../containers/VisibileTodoList';
+import VisibleTodoList from '../containers/VisibleTodoList';
+import '../App.css'
 
 const App = () => (
-  <div>
+  <div className="App">
+    <h1>Todo App</h1>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
